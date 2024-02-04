cf19f064715be3882349e55d098b79499b5784a3
Vassiliy Girichev <vassiliy.girichev@3smoneyclubltd.onmicrosoft.com>
2024-02-04 22:07:16 +0200

    test draft

--- test/hackmd.opernapi.test.ts
+++ test/hackmd.opernapi.test.ts
@@ -6,7 +6,7 @@ describe('UserApi test suite', () => {
     
     test('check user details', async () => {
         let userApi = new hackMdAPI.UserApi()
-        userApi.accessToken = "36O9WEBA5JBCHL9BQHM0K8DO6KRURC3VXRRG05TE0MCOC8ZOI1"
+        userApi.accessToken = "<access token>"
         let userData = await userApi.getUserData()
 
         console.log(userData)
